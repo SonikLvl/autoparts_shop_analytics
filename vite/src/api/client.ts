cd8ed901e65c8478b7f8261f45695f868e78a287
SonikLvl <sofiy.kulichenko@gmail.com>
2026-04-15 16:39:12 +0300

    lab3: docker + compose

--- vite/src/api/client.ts
+++ vite/src/api/client.ts
@@ -10,7 +10,7 @@ import {
 } from '../types/api';
 
 const USE_MOCK = false; // Toggle this to switch to real API
-const API_BASE_URL = 'http://209.38.201.190:5000/api/analytics';
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 export const apiClient = {
   async getSuppliers(category?: string, productName?: string, minVolume?: number, startDate?: string, endDate?: string): Promise<Supplier[]> {
